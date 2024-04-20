62364389967c795ca5db19ca3952acc8856e0ab3
MoAlkhateeb <mo.hany.alkhateeb@gmail.com>
2024-04-20 17:17:34 +0200

    showcase

--- src/scheduler.c
+++ src/scheduler.c
@@ -4,9 +4,7 @@
 int main(int argc, char * argv[])
 {
     initClk();
-    
     //TODO implement the scheduler :)
     //upon termination release the clock resources.
-    
     destroyClk(true);
 }
